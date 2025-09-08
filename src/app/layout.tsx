c588a8d4124f69171c5f09dfeb9e3520cd36837c
andy70123 <lauandy@gmail.com>
2025-09-08 09:29:44 +0600

    Update layout.tsx
    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,11 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <meta httpEquiv="Cache-Control" content="public, max-age=31536000" />
+        <meta httpEquiv="Connection" content="keep-alive" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </head>
       <body className="antialiased">
         <Navbar />
